bf8cf30670ec596a184d8ad03abd1e29ef324b88
henry perlman <bperlman1@gmail.com>
2016-03-04 13:19:05 -0500

    first commit of stratbase - more to come

---
+++ pystrats/stratbase.py
@@ -0,0 +1,21 @@
+'''
+Created on Mar 4, 2016
+    Base def's for strategies that are both intra day and daily
+@author: bperlman1
+'''
+
+def basic_strat(df=None,begYyyyMmDd,endYyyyMmDd):
+    ''' 
+    base strategy
+    '''
+    
+def interate_strats(dfList):
+    '''
+    loop through df's in dflist and run a subclass of basic_strat
+      Ret
+    '''
+    for df in dfList:
+        
+
+
+    
